0d3109ff9cf418b7edc706bd23ba2188c6ecf97f
Владимир Филиппов <markatripsdl@gmail.com>
2022-05-17 19:19:44 +0500

    add Github Actions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
-#!/usr/bin/env node
-
 import {name} from "../src/cli.js";
+
+#!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
 name();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const name = () =>  {
-    const getUser = readlineSync.question('May I have your name? ');
-   
-    console.log('Hello, ' + getUser + '!');
-}
-
+export const name = () => {
+  const getUser = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + getUser + '!');
+};
